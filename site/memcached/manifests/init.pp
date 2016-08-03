23f2312b6ab5b62df81ce888aa5481eb6210fef2
kimhouse <housek@us.ibm.com>
2016-08-03 13:33:56 -0400

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,24 @@
+class memchaced{
+   package {'memcached':
+     ensure => present
+   }
+   
+   
+   file {'/etc/sysconfig/memcached':
+     ensure => file,
+     owner => 'root',
+     group => 'root',
+     port => '11211',
+     user => 'memcached',
+     maxconn => '96',
+     cachesize => '32',
+     require => Package ['memcached'],
+   }
+   
+   
+   service {'memcached':
+    ensure => running,
+    enable => true,
+   
+   
+   }
