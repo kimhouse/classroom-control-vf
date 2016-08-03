3820d8323925fe9385f150a1cae9c20c1f16f389
kimhouse <housek@us.ibm.com>
2016-08-03 10:54:29 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,11 +1,17 @@
 class skeleton {
 file {'/etc/skel':
-  ensure => directory,
+   ensure => directory,
+   owner => 'root',
+   group => 'root',
+   mode => '0755',
 }
 
 file {'/etc/skel/bash.rc':
   ensure => file,
-}
+  onwer => 'root',
+  group => 'root',
+  source => 'puppet:///modules/skeleton/bashrc',
+  }
 }
 
 
