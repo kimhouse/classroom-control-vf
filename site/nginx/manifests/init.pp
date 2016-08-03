2de078cb734437bb7821f34f67db657b432a6c27
kimhouse <housek@us.ibm.com>
2016-08-03 15:06:50 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,6 +1,38 @@
 class nginx {
 
+package {'nginx':
+ ensure => present,
+}
 
+file {'/etc/nginx/nginx.conf':
+ ensure => file,
+ owner => 'root',
+ group => 'root',
+ }
+ 
+file {'/etc/nginx/conf.d/default.conf':
+ ensure => file,
+ owner => 'root',
+ group => 'root',
+ source => puppet:///modules/nginx/default.conf
+ }
 
+file {'var/www':
+ ensure => directory,
+ owner => 'root',
+ group => 'root',
+}
+
+file {'var/www/index.html':
+ ensure => file,
+ owner => 'root',
+ group => 'root',
+ source => puppet:///modules/nginx/index.html,
+}
+
+service {'nginix':
+ ensure => running,
+ enable => true,
+}
 
 }
