a2c248a209d0f42018846b9840b7ae0591982d6a
kimhouse <housek@us.ibm.com>
2016-08-04 10:22:15 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -31,7 +31,7 @@ file {'/var/www/index.html':
 service {'nginx':
  ensure => running,
  enable => true,
- subscribe => [ File['default.conf'], File['ngnix.conf']],
+ subscribe => [ File['/etc/nginx/conf.d/default.conf'], File['/etc/nginx/ngnix.conf']],
  
 }
 
