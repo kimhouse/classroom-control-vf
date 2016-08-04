4384f14388e8ececac66a3c7fdc8ca94165566d2
kimhouse <housek@us.ibm.com>
2016-08-04 13:37:09 -0400

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,4 +1,16 @@
-user:managed_user {
- user => 
- home = >
+define users::managed_users (
+ $group = $title,
+) {
  
+ user { $title
+  ensure => present,
+ }
+ 
+ file { "/home/${title}":
+  ensure => directory
+  owner => $title
+  group => $group
+ 
+ }
+
+}
