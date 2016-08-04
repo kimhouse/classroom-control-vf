8bc8181ad73c2020d32640050c34ca558de54e98
kimhouse <housek@us.ibm.com>
2016-08-04 13:42:20 -0400

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,1 +1,19 @@
+class users::admins {
+
+users::managed_user {'jose': }
+users:managed_user {'alice':
+ group => 'superusers',
+}
+users:managed_user {'chen':}
+
+group { 'superusers':
+ ensure => present
+ 
+ }
+
+
+
+
+}
+
 
